c246f9cf732a7d6fb341087747602807f3147cad
Louis Amoros <lamoros@bmat.com>
2024-01-19 14:24:24 +0100

    setup LLM usage with a boolean

--- LLMAgent/src/GPT.py
+++ LLMAgent/src/GPT.py
@@ -1,4 +1,7 @@
-from gpt4all import GPT4All
+useLLM = True
+
+if useLLM:
+    from gpt4all import GPT4All
 
 basePrompt = "You are a helpful assistant called Jarvis who was created by a group of student in a school project for the company Ingescape."
 
@@ -10,7 +13,14 @@ def buildPrompt(prompt: str):
 def handlePrompt(prompt: str):
     fullPrompt = buildPrompt(prompt)
 
+    if useLLM:
+        return handlePromptLLM(fullPrompt)
+    else:
+        return "A basic response from Jarvis"
+
+
+def handlePromptLLM(prompt: str):
     model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-    output = model.generate(fullPrompt, max_tokens=50)
+    output = model.generate(prompt, max_tokens=50)
 
     return output
