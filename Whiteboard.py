e2abd6b51326cb78ab6ea0dd3df5a6b3c1610aef
Louis Amoros <lamoros@bmat.com>
2024-01-31 12:40:24 +0100

    add incomplete printGif function

--- Whiteboard.py
+++ Whiteboard.py
@@ -1,4 +1,10 @@
 import ingescape as igs
 
-def printMessage(message) : 
-    igs.service_call("Whiteboard", "chat", (message),"")
+
+def printMessage(message):
+    igs.service_call("Whiteboard", "chat", (message), "")
+
+
+def printGif():
+    print("NOT DONE YET")
+    # igs.service_call("Whiteboard", "gif", (message), "")
